05dcc04899a67ab197f43a8436af529a31b32019
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-05-27 10:43:19 +0530

    Time: 336 ms (45.44%), Space: 146.3 MB (11.59%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -1,25 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if(root == NULL) return 0;
-         queue<TreeNode*> Q;
-        Q.push(root);
-        int i=0;
-        while(!Q.empty()){
-            i++;
-            int k = Q.size();
-            for (int j=0;j<k;j++){
-                TreeNode* rt = Q.front();
-                if(rt->left) Q.push(rt->left);
-                if(rt->right) Q.push(rt->right);
-                Q.pop();
-                if(rt->left==NULL && rt->right==NULL) return i;
-            }
-        }
-        return -1;
-    }       
+        int left, right;
+        if(root == nullptr) return 0;
+        else if(root->left == nullptr && root->right == nullptr) return 1;
+        if(root->left !=nullptr) left = minDepth(root->left);
+        else left = INT_MAX;
+        if(root->right!=nullptr) right = minDepth(root->right);
+        else right = INT_MAX;
+        return 1+min(left, right);
+    }
 };
-
-
-
-
