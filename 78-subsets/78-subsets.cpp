53c430e22ea9146aa35e3c42c33420b159b5f534
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-06-04 13:57:35 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (69.14%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     
-    vector<vector<int>> call(vector<int> &nums, int i){
-        if(i>=nums.size()) return {{}};
-     vector<vector<int>> par = call(nums, i+1);
-        vector<vector<int>>ans;
-        for (auto j:par) ans.push_back(j);
-        for (auto j:par){
-            j.push_back(nums[i]);
-            ans.push_back(j);
+    void call(vector<int> &nums, int i, vector<int> &subset, vector<vector<int>> &ans ){
+        if(i>=nums.size())
+        {
+            ans.push_back(subset); return;
         }
-        return ans;
-    }
+        subset.push_back(nums[i]);
+        call(nums, i+1, subset, ans);
+        subset.pop_back();
+        call(nums, i+1, subset, ans);
+}
+    
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        return call(nums, 0);
+        vector<vector<int> > ans;
+        vector<int> subset;
+        call(nums, 0, subset, ans);
+        return ans;
     }
 };
     
