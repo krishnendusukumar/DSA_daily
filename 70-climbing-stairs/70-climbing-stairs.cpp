cfd5f5a10198c801e1a7b21a12ef015bc6df5fde
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-06-09 04:05:05 +0530

    Time: 4 ms (21.37%), Space: 5.9 MB (56.38%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2 = 1;
+        int prev = 1;
+        for (int i=2;i<=n;i++){
+            long long element = prev + prev2;
+            prev2 = prev, prev = element;
+        }
+        return prev;
+    }
+};
