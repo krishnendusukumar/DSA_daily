5351dc153d39dcfa7315c5417743e55589786559
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-06-24 02:15:33 +0530

    Time: 23 ms (36.55%), Space: 52 MB (27.86%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String a, String b) {
+        HashMap<Character,Integer> m = new HashMap<Character,Integer>();
+        for(int i=0;i<b.length();i++){
+            char c = b.charAt(i);
+            if(m.containsKey(c)){
+                m.put(c, m.get(c)+1);
+            }
+            else m.put(c, 1);
+        }
+         for(int i=0;i<a.length();i++){
+            char c = a.charAt(i);
+            if(m.containsKey(c))
+            {
+                if(m.get(c)<=0)
+                    return false;
+                else
+                    m.put(c,m.get(c)-1);
+            }                
+            else
+                return false;
+        }
+        return true;
+    }
+}
