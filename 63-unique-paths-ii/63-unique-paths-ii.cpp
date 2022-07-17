00af5a50393fc3993339a83c3a19a317ca0b783e
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-07-17 16:24:20 +0530

    Time: 5 ms (48.48%), Space: 7.9 MB (42.94%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -13,6 +13,19 @@ public:
     
     int uniquePathsWithObstacles(vector<vector<int>>& o) {
         vector<vector<int>> dp(o.size(), vector<int>(o[0].size(), -1));
-        return helper(o.size()-1, o[0].size()-1, o, dp);
+        dp[0][0] = 1;
+        for (int i = 0; i < o.size(); i++) {
+            for(int j = 0 ; j < o[0].size(); j++) {
+                if(o[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    int up = 0, right = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) right = dp[i][j-1];
+                    dp[i][j] = up + right;
+                }
+            }
+        }
+        return dp[o.size()-1][o[0].size()-1];
     }
 };
