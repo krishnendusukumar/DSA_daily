2431c65772136b53d254b1bde26ddbef4a285aa3
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-07-18 11:07:43 +0530

    Time: 9 ms (57.59%), Space: 8.5 MB (88.92%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -11,8 +11,12 @@ public:
     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
-        
-vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(), -1));
-            return helper(0, 0, triangle, dp);
+        vector<int> dp(triangle.back());
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+                    dp[j]=triangle[i][j]+min(dp[j], dp[j+1]);
+            }
+        }
+        return dp[0];
     }
 };
