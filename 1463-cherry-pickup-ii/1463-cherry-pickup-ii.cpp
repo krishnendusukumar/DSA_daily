1733eb57bcc88fda8c68172ccbe0a65a5c9c83f5
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-07-19 15:51:22 +0530

    Time: 511 ms (5.05%), Space: 17 MB (15.39%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -23,6 +23,30 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m,-1)));
-        return helper(0 , 0 , m-1 , grid, dp);
+        for(int j1 = 0; j1 < grid[n-1].size(); j1++) {
+            for(int j2 = 0; j2 < grid[n-1].size(); j2++) {
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        for(int i = n-2; i >= 0; i--) {
+            for(int j1 = 0; j1 < grid[i].size(); j1++) {
+                for (int j2 = 0; j2 < grid[i].size(); j2++){
+                    int maxi = -1e9;
+                    for(int k1 = -1; k1 <= 1; k1++) {
+                        for(int k2 = -1; k2 <= 1; k2++){
+                        int value = 0;
+                if(j1 == j2 ) value =  grid[i][j2];
+                else value = grid[i][j2] + grid[i][j1];
+                        if(j1 + k1 >= 0 && j1 + k1 < grid[i].size() && j2 + k2 >= 0 && j2 + k2 < grid[i].size()) value += dp[i+1][j1 + k1][j2 + k2];
+                        else value += -1e9;
+                        maxi = max(maxi , value);
+            }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
