6c2082d87be53758a26bf3de9790db5743eefa48
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-08-01 11:42:19 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     
-    int helper(int i, int j, vector<vector<int>>& dp) {
-        if(i == 0 && j == 0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int left = helper(i, j-1, dp);
-        int right = helper(i-1, j, dp);
-        return dp[i][j] = left + right;
+    int helper(int m, int n, vector<vector<int>>& dp) {
+        if(m == 0 && n==0) return 1;
+        if(m < 0 || n < 0) return 0;
+        if(dp[m][n] != -1) return dp[m][n];
+        
+        int left = helper(m, n-1, dp);
+        int right = helper(m-1, n, dp);
+        
+        return dp[m][n] = left + right;
     }
     
     int uniquePaths(int m, int n) {
-        vector<int> v(n, 0), dp(n, 0);
-        
-        dp[0] = 1;
-            for(int i = 0;i < m;i++) {
-                for(int j = 0; j < n; j++) {
-                    if(i == 0 && j == 0) dp[j] = 1;
-                    else {int left = 0;
-                    int right = 0;
-                    if(i > 0) right = v[j];
-                    if(j>0) left = dp[j-1];
-                    dp[j] = left + right;}
-                }
-                v = dp;
-            }
-        return dp[n-1];
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return helper(m-1, n-1, dp);
     }
 };
