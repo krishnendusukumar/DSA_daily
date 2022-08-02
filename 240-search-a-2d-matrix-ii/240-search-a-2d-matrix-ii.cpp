142c2c90e1fd5deb46c22a67dd240f11e8834fab
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-08-02 13:36:23 +0530

    Time: 148 ms (75.80%), Space: 14.9 MB (21.69%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,37 +1,16 @@
 class Solution {
 public:
-    
-    bool helper(vector<vector<int>>& matrix, int target, int i, int j, int m) {
-        int low = i, high = j-1;
-        while(low <= high) {
-            int mid = low + (high - low)/2;
-            if(matrix[mid][m] == target) return true;
-            else if(matrix[mid][m] > target) high = mid - 1;
-            else if(matrix[mid][m] < target) low = mid+1;
-        }
-        return false;
-    }
-    
-    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
         
-        int l = 0, h = m-1;
-        int jmax = h;
-        while(l <= h) {
-            int mid = l + (h-l)/2;
-            if(matrix[0][mid] == target) return true;
-            else if(matrix[0][mid] > target) {
-                jmax = mid-1;
-                h = mid - 1;
-            }
-            else l = mid + 1;
-        }
-        int j = 0, i = 0;
-        while(j <= jmax) {
-            if(helper(matrix, target, 0, n, j)) return true;
-            else j++;
+        int row = 0;
+        int col = m-1;
+        
+        while(row < n && col >= 0) {
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else row++;
         }
         return false;
     }
