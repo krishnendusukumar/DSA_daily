ee92037c064d95e8f3762f4a999d60d142f9449e
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-08-02 13:21:32 +0530

    Time: 571 ms (8.80%), Space: 14.9 MB (21.69%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,12 +1,37 @@
 class Solution {
 public:
+    
+    bool helper(vector<vector<int>>& matrix, int target, int i, int j, int m) {
+        int low = i, high = j-1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(matrix[mid][m] == target) return true;
+            else if(matrix[mid][m] > target) high = mid - 1;
+            else if(matrix[mid][m] < target) low = mid+1;
+        }
+        return false;
+    }
+    
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = m ? matrix[0].size() : 0, r = 0, c = n - 1;
-        while (r < m && c >= 0) {
-            if (matrix[r][c] == target) {
-                return true;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int l = 0, h = m-1;
+        int jmax = h;
+        while(l <= h) {
+            int mid = l + (h-l)/2;
+            if(matrix[0][mid] == target) return true;
+            else if(matrix[0][mid] > target) {
+                jmax = mid-1;
+                h = mid - 1;
             }
-            matrix[r][c] > target ? c-- : r++;
+            else l = mid + 1;
+        }
+        int j = 0, i = 0;
+        while(j <= jmax) {
+            if(helper(matrix, target, 0, n, j)) return true;
+            else j++;
         }
         return false;
     }
