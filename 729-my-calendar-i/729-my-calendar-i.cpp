b19e6dc504d47af271b57e7981be980371d71b2c
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-08-03 13:39:20 +0530

    Time: 109 ms (86.61%), Space: 38.7 MB (60.92%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,18 +1,17 @@
 class MyCalendar {
     private: map<int,int> m;
-    
-    
-    
 public:
     MyCalendar() {
     }
     
     bool book(int start, int end) {
-        for(auto it: m) {
-            if(!(start >= it.second || end <= it.first)) return false;
+        auto  it = m.upper_bound(start);
+        
+        if(it == m.end() || end <= it->second) {
+            m[end] = start;
+            return true;
         }
-        m[start] = end;
-        return true;
+        else return false;
     }
 };
 
