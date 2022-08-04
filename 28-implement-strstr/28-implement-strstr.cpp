dc75d84afce2c28930d27b462373011766e4593a
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-08-04 17:25:11 +0530

    Time: 4 ms (32.22%), Space: 6.3 MB (61.85%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    int strStr(string h, string n) {
-        if(h.size() < n.size()) return -1;
-        
-        for(int i=0;i <= h.size() - n.size(); i++) {
-           bool flag = true;
-            for(int j = 0; j < n.size();j++) {
-                if(n[j]!=h[j+i]) flag = false;
+    
+    
+    void constructLps(string n, vector<int>& lps) {
+        int len = 0, i = 1;
+        lps[0] = 0;
+        while(i < n.size()) {
+            if(n[i] == n[len]) {
+                lps[i++] = ++len;
             }
-            if(flag) return i;
+            else {
+                if(len > 0) len = lps[len-1];
+                else lps[i++] = 0;
+            }
+        }
+    }
+    
+    
+    int strStr(string h, string n) {
+           vector<int> lps(n.size());
+        constructLps(n, lps);
+        int i = 0, j = 0;
+        while(i < h.size() && j < n.size()) {
+            if(h[i] == n[j]) 
+                i++,j++;
+            if(j == n.size()) return i-j;
+            else if(i < h.size() && h[i] != n[j])
+                j ? j = lps[j-1] : i++;
         }
         return -1;
     }
