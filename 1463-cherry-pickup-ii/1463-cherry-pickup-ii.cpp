dc509502d05af05f5b511c856d3015ee1f9708f6
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-08-15 01:29:32 +0530

    Time: 172 ms (46.82%), Space: 17 MB (30.03%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,53 +1,30 @@
 class Solution {
 public:
     
-    int helper(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
-        if(j1 < 0 || j1 > grid[i].size()-1 || j2 < 0 || j2 > grid[i].size()-1) return -1e9;
+    int helper(vector<vector<int>>& grid, vector<vector<vector<int>>>& dp, int i, int j1, int j2) {
+        if(j1 > grid[i].size()-1 ||  j1 < 0 || j2 > grid[i].size()-1 || j2 < 0) return -1e9;
         else if(i == grid.size()-1) {
-            if(j1 == j2 ) return grid[i][j1];
+            if(j1 == j2) return grid[i][j1];
             else return grid[i][j1] + grid[i][j2];
         }
         else if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
         int maxi = -1e9;
         for(int k1 = -1; k1 <= 1; k1++) {
             for(int k2 = -1; k2 <= 1; k2++) {
-                if(j1 == j2 ) maxi = max(maxi , grid[i][j2] + helper(i+1, j1+k1, j2+k2, grid, dp));
-                else maxi = max(maxi , grid[i][j2] + grid[i][j1] + helper(i+1, j1+k1, j2+k2, grid, dp));
+                int value = 0;
+                if(j1 == j2) value = grid[i][j1];
+                else value = grid[i][j1] + grid[i][j2];
+                value += helper(grid, dp, i+1, j1+k1, j2+k2);
+                maxi = max(maxi, value);
             }
         }
-        return dp[i][j1][j2] = maxi;
-        
+        return dp[i][j1][j2] = maxi; 
     }
     
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(m, -1)), temp = dp;
-        for(int j1 = 0; j1 < grid[n-1].size(); j1++) {
-            for(int j2 = 0; j2 < grid[n-1].size(); j2++) {
-                if(j1 == j2) temp[j1][j2] = grid[n-1][j1];
-                else temp[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
-            }
-        }
-        for(int i = n-2; i >= 0; i--) {
-            for(int j1 = 0; j1 < grid[i].size(); j1++) {
-                for (int j2 = 0; j2 < grid[i].size(); j2++){
-                    int maxi = -1e9;
-                    for(int k1 = -1; k1 <= 1; k1++) {
-                        for(int k2 = -1; k2 <= 1; k2++){
-                        int value = 0;
-                if(j1 == j2 ) value =  grid[i][j2];
-                else value = grid[i][j2] + grid[i][j1];
-                        if(j1 + k1 >= 0 && j1 + k1 < grid[i].size() && j2 + k2 >= 0 && j2 + k2 < grid[i].size()) value += temp[j1 + k1][j2 + k2];
-                        else value += -1e9;
-                        maxi = max(maxi , value);
-            }
-                    }
-                    dp[j1][j2] = maxi;
-                }
-            }
-            temp = dp;
-        }
-        return dp[0][m-1];
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m,-1)));
+        return helper(grid, dp, 0, 0, grid[0].size()-1);
     }
 };
