ecf2a4394f77a2d7745f5d4ad2d072433c5feacf
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-08-15 00:34:28 +0530

    Time: 14 ms (92.78%), Space: 10.2 MB (65.92%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     
-    int helper(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-        if(j >= triangle[0].size() || j < 0 ) return 1e9;
-        if(i == 0) return triangle[i][j];
-        else if(dp[i][j] != -1) return dp[i][j];
-        int left = 1e9, right = 1e9, down = 1e9;
-        left =  helper(i-1, j, triangle, dp);
-        right = helper(i-1, j+1, triangle, dp);
-        down = helper(i-1, j-1, triangle, dp);
-        return  dp[i][j] = triangle[i][j] + min(down,min(left, right));
+    int helper(vector<vector<int>>& matrix, vector<vector<int>>& dp, int i, int j) {
+        if(i == matrix.size()-1) return matrix[i][j];
+        if(i > matrix.size()-1 || j > matrix.size()-1 || j < 0) return 1e9;
+        if(dp[i][j]!= -1) return dp[i][j];
+        
+        int left = 1e9, right = 1e9, bottom = 1e9;
+        if(j > 0) left = helper(matrix, dp, i+1, j-1);
+        if(j < matrix.size()-1) right = helper(matrix, dp, i+1, j+1);
+        bottom = helper(matrix, dp, i+1, j);
+        return dp[i][j] = matrix[i][j] + min(bottom, min(left, right));
     }
-    
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<int> dp(matrix.front()), prev(n, 0);
-        int maxc = 1e9;
-        for(int i=1; i < matrix.size(); i++) {
-            for(int j = 0; j < matrix[i].size(); j++) {
-                int left = 1e9, right = 1e9, down = 1e9;
-                left = dp[j];
-                if(j > 0) right = dp[j-1];
-                if(j < matrix[i].size()-1) down = dp[j+1];
-                prev[j] = matrix[i][j] + min(left, min(right, down));
-            }
-            dp = prev;
+        vector<vector<int>> dp(matrix.size(), vector<int>(matrix.size(), -1));
+        int maxi = 1e9;
+            for(int j = 0 ; j < matrix.size(); j++) {
+            maxi = min(maxi, helper(matrix, dp, 0, j));
         }
-       return *min_element(dp.begin(), dp.end());
-    }
+        return maxi;
+        }
 };
