ed12f0dd509cc75e3e14c83cfcfc4cc3ba94dca1
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-09-18 12:33:44 +0530

    Time: 36 ms (53.57%), Space: 13.1 MB (24.32%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -13,7 +13,18 @@ public:
         int n = text1.size();
         int m = text2.size();
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return helper(text1, text2, n-1, m-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int i= 0; i < n; i++) dp[i][0]  = 0;
+        for(int j = 0 ; j < m; j++) dp[0][j] = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
