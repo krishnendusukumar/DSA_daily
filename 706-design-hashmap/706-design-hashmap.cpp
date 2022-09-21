843031df6dd8b8833f2efa9faf551d98c6167b2f
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-09-22 03:14:05 +0530

    Time: 177 ms (79.85%), Space: 53.2 MB (68.20%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -22,29 +22,15 @@ public:
 
     // search(key) returns an iterator pointing to the key, else we return an iterator pointing to the End of the LinkedList
 
-    list<pair<int , int>> :: iterator search(int key)
+        list<pair<int,int>> :: iterator search(int key) 
     {
-        // We first calculate the index i using hash(key) function
-
-        int i = hash(key) ;
-
-        // We cannot directly use find() function because we know the key but we don't know the value associated with the key
-
-        list<pair<int , int>> :: iterator it = m[i].begin() ;
-
-        while(it != m[i].end() )
-        {
-            // If at any moment our iterator -> first becomes equal to key, we return it
-
-            if(it -> first == key)
-                return it ;
-
-            it++ ;
+        int i = hash(key);
+        list<pair<int,int>> :: iterator it = m[i].begin();
+        while(it != m[i].end()) {
+            if(it->first == key) return it;
+            it++;
         }
-
-        // At the end, we return it pointing to the end of the LinkedList of m[i]
-
-        return it ;
+        return it;
     }
 
     // put(key , value) inserts the Value with it's corresponding Key
