ec15e863e67d7df4a564579bd6c261b7056cd82b
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-09-22 02:39:23 +0530

    Time: 196 ms (48.14%), Space: 41.1 MB (76.88%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,98 +1,41 @@
 class MyHashSet {
 public:
 
-   vector<list<int> > m ;
-int siz ;
-
-public:
-    // Inside the constructor we initialise our siz variable with 100(or any number of our wish) and resize our vector<list<int>> m according to our siz
-
-    MyHashSet()
-    {
-        siz = 100 ;
-
-        m.resize(siz) ;
+  
+    
+    vector<list<int>> m;
+    int size;
+    
+    MyHashSet() {
+        size = 100;
+        m.resize(size);
     }
-
-    // Our hash function simply returns the index by taking modulo with siz variable
-
-    int hash(int key)
-    {
-        return (key % siz) ;
+    
+    int hash(int key) {
+        return (key%size);
     }
-
-    // Search() functions returns us an iterator indicating the position where the Key is present
-
-    list<int> :: iterator search(int key)
-    {
-        int i = hash(key) ;
-
-        // m[i] gives us the entire Doubly LinkedList present at i-th Index
-        // find() function is a STL function which needs 3 variables to search for a key:
-        // a) Staring iterator:- m[i].begin()
-        // b) Ending  iterator:- m[i].end()
-        // c) Key to be searched: key
-
-        return find(m[i].begin() , m[i].end() , key) ;
+    
+    list<int> :: iterator search(int key) {
+        int i = hash(key);
+        return find(m[i].begin(), m[i].end(), key);
     }
-
-    // add() function adds the given Key into our vector<list<int>> m at the index specified by our hash() function
-
-    void add(int key)
-    {
-        // If the key is already present, we don't need to add it again
-
-        if(contains(key) )
-            return ;
-
-        // Otherwise, we will find the index i where we need to add our key using hash() function
-
-        int i = hash(key) ;
-
-        // We add the key at the end in O(1) Time Complexity
-
-        m[i].push_back(key) ;
-
+    
+    void add(int key) {
+        if(contains(key)) return;
+        int ind = hash(key);
+        m[ind].push_back(key);
     }
-
-      // remove() function deletes the given Key from our vector<list<int>> m
-
-    void remove(int key)
-    {
-        // Firstly, we need to check whether our key is present inside our vector<lis<int>>. If the key is not present, we cannot delete it, thus we directly return
-
-        if(!contains(key) )
-            return ;
-
-        // We find the index i using our hash() function
-
-        int i = hash(key) ;
-
-        // m[i] is the total Linked-list in which our key is present
-        // search(key) returns us the iterator denoting the position where our key is present in that List
-        // erase() function deletes that key from the List
-
-        m[i].erase(search(key) ) ;
-
+    
+    void remove(int key) {
+        if(!contains(key)) return;
+        int i = hash(key);
+        m[i].erase(search(key));
     }
-
-    // contains() function returns true if the key is present else returns false
-
-    bool contains(int key)
-    {
-        // We first calculate the index i where our key might be present using hash() function
-
-        int i = hash(key) ;
-
-        // Since search(key) returns us an iterator. If that iterator is not equal to m[i].end() , it indicates that the element is present, thus we return true
-
-        if(search(key) != m[i].end() )
-            return true ;
-
-        // Else the element is not present, thus we return false
-
-        else
-            return false ;
+    
+    bool contains(int key) {
+        int i = hash(key);
+        if(search(key) == m[i].end()) return false;
+        return true;
     }
 };
 
