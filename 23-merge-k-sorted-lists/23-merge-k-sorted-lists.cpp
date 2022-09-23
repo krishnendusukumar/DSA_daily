eeea26a705126c53f11c503bfb7ad6174f7953e5
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-09-23 16:04:40 +0530

    Time: 213 ms (24.33%), Space: 13 MB (95.85%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -12,63 +12,34 @@ class Solution {
 public:
     
     ListNode* addition(ListNode* l1, ListNode* l2) {
-        if(!l1) return l2;
-        if(!l2) return l1;
-        
         ListNode *ans, *tail;
-        if(l1->val < l2->val) 
-        {
-            ans = l1;
-            tail = l1;
-            l1 =  l1->next;  
-        }
-        else {
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        
+        if(l1 -> val > l2->val) {
             ans = l2;
             tail = l2;
-            l2  = l2->next;  
-        } 
-
-        while(l1 != NULL && l2 != NULL)
-    {
-        // If value pointed by l1 is smaller than l2's value
-
-        if(l1 -> val < l2 -> val)
-        {
-            // We make Tail's Next point to l1
-
-            tail -> next = l1 ;
-
-            // We move tail to l1
-
-            tail = l1 ;
-
-            // l1  moves to it's Next Node
-
-            l1 = l1 -> next ;
+            l2 = l2 -> next;
         }
-
-        // Otherwise, value pointed by l2 is smaller than or equal to l1's value
-
-        else
-        {
-            // We make Tail's Next point to l2
-
-            tail -> next = l2 ;
-
-            // We move tail to l2
-
-            tail = l2 ;
-
-            // l2 moves to it's Next Node
-
-            l2 = l2 -> next ;
-
+        else {
+            ans = l1;
+            tail = l1;
+            l1 = l1->next;
         }
-    }
-
-        if (l1 == nullptr) {
-            tail -> next = l2;
+        
+        while(l1 && l2) {
+            if(l1 -> val > l2 -> val) {
+                tail -> next = l2;
+                tail = l2;
+                l2 = l2->next;
+            }
+            else {
+                tail -> next = l1;
+                tail = l1;
+                l1 = l1 -> next;
+            }
         }
+        if(l1 == nullptr) tail -> next = l2;
         else tail -> next = l1;
         return ans;
     }
