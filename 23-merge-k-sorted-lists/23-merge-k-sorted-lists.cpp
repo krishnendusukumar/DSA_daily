808b3b37a7fd28c9b0b2fc452940ad01cc3245b8
Krishnendu Sukumar <70630267+krishnendusukumar@users.noreply.github.com>
2022-09-23 16:54:40 +0530

    Time: 31 ms (78.30%), Space: 13.2 MB (76.61%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,49 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    
-    ListNode* addition(ListNode* l1, ListNode* l2) {
-        ListNode *ans, *tail;
-        if(l1 == nullptr) return l2;
-        if(l2 == nullptr) return l1;
-        
-        if(l1 -> val > l2->val) {
-            ans = l2;
-            tail = l2;
-            l2 = l2 -> next;
-        }
-        else {
-            ans = l1;
-            tail = l1;
-            l1 = l1->next;
-        }
-        
-        while(l1 && l2) {
-            if(l1 -> val > l2 -> val) {
-                tail -> next = l2;
-                tail = l2;
-                l2 = l2->next;
-            }
-            else {
-                tail -> next = l1;
-                tail = l1;
-                l1 = l1 -> next;
-            }
-        }
-        if(l1 == nullptr) tail -> next = l2;
-        else tail -> next = l1;
-        return ans;
+class cmp{
+    public:
+     bool operator() (ListNode *a, ListNode *b) {
+        return (a->val > b-> val);
     }
+};
+
+class Solution {
+
+    
+    
+    public:
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size() == 0) return nullptr;
-        ListNode *add, *first;
-        add = lists[0];
-        for(int i = 1; i < lists.size(); i++) {
-            add = addition(add, lists[i]);
+        priority_queue<ListNode*, vector<ListNode*>, cmp> q;
+        ListNode* ans, *dummy = new ListNode(-1);
+        ans = dummy;
+        for(int i = 0; i < lists.size(); i++) {
+            if(lists[i]) {
+                q.push(lists[i]);
+            }
         }
-        return add;
+        while(q.size()) {
+            ListNode *temp = q.top();
+            q.pop();
+            ans -> next = temp;
+            ans = temp;
+            if(temp->next) q.push(temp->next);
+        }
+        return dummy -> next;
     }
 };
